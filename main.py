31215ac13a71f935f1320e164cad43f1b9192f0b
Dawid <davi12396@gmail.com>
2018-03-01 18:15:58 +0100

    Parsers, cellularAuto, Eval

---
+++ main.py
@@ -0,0 +1,11 @@
+from OutputParser import OutputParser
+from InputParser import InputParser
+
+PATH = "E:\\GoogleHashCode2018\\"
+FILE = "example"
+
+inputData = InputParser(PATH + FILE + ".in")
+
+outputData = OutputParser(PATH + FILE + ".out")
+
+
