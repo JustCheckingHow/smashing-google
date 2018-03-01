919e903bc7340ab288b075af3dab7cfcc66058ff
Dawid <davi12396@gmail.com>
2018-03-01 19:06:45 +0100

    Auto class

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ FILE = "a_example"
 class Auto:
     def __init__(self):
         self.ticksToFree = 0
+        self.x = 0
+        self.y = 0
 
     def goRide(self, ride):
-        pass
+        self.x = ride[2]
+        self.y = ride[3]
+
+    def tick(self):
+        if self.ticksToFree > 0:
+            self.ticksToFree -= 1
 
 inputData = InputParser(PATH + FILE + ".in")
 rows, columns, vehicles, numebrOfRides, bonusOnTime, steps = inputData.readLine()
