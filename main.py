8f98c7aa395e63c46c8baa38b1d759337d751324
Dawid <davi12396@gmail.com>
2018-03-01 19:00:21 +0100

    Parsing input

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ from OutputParser import OutputParser
 from InputParser import InputParser
 
 PATH = "E:\\GoogleHashCode2018\\"
-FILE = "example"
+FILE = "a_example"
+
+class Auto:
+    def __init__(self):
+        self.ticksToFree = 0
+
+    def goRide(self, ride):
+        pass
 
 inputData = InputParser(PATH + FILE + ".in")
+rows, columns, vehicles, numebrOfRides, bonusOnTime, steps = inputData.readLine()
+rides = []
+for i in range(steps):
+    rides.append(inputData.readLine())
+
+inputData.close()
 
 outputData = OutputParser(PATH + FILE + ".out")
-
+outputData.close()
 
